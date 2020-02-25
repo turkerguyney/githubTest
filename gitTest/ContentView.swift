e7654f6e0cfe599ccdbb3acd89fa03983f156144
Turker Guney <turkerguney@gmail.com>
2020-02-25 18:18:24 +0300

    Initial Commit

---
+++ gitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTest
+//
+//  Created by Turker Bilge Guney on 25.02.2020.
+//  Copyright © 2020 Turker Bilge Guney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
