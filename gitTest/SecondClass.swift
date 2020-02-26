53f3df3c99a68891513d205317462ce4bb3b2003
Turker Guney <turkerguney@gmail.com>
2020-02-26 16:32:48 +0300

    second class

---
+++ gitTest/SecondClass.swift
@@ -0,0 +1,14 @@
+//
+//  SecondClass.swift
+//  gitTest
+//
+//  Created by Turker Bilge Guney on 26.02.2020.
+//  Copyright © 2020 Turker Bilge Guney. All rights reserved.
+//
+
+import Foundation
+
+
+class SecondClass {
+    
+}
